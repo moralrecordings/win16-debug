99d36430c96031cea72711bbc724c70f863f0426
Scott Percival <code@moral.net.au>
2019-09-05 16:15:36 +0800

    get_segtable: output JSON

--- get_segtable.py
+++ get_segtable.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import argparse
+import json
+import ntpath
 
 from mrcrowbar import models as mrc, utils
 from mrcrowbar.lib.hardware import ibm_pc
@@ -44,20 +46,19 @@ def get_module_table( memory_map, offset ):
 LDT_BASE, LDT_LIMIT = 0x80BAD000, 0x2FFF
 
 DESCRIPTION = 'Extract the Win16 segment table from a DOSBox memory dump.'
-EPILOG = """Windows 3.1 uses a single shared segment table for all programs. In order to use the DOSBox debugger, you will need to know which segment ID maps to a segment in the target EXE or DLL. This tool scrapes this mapping from memory dumps of a running application
+EPILOG = """Windows 3.1 uses a single shared segment table for all programs. In order to use the DOSBox debugger, you will need to know which 16-bit selector maps to each segment in the target EXE or DLL. This tool scrapes this mapping from DOSBox memory dumps taken with a running application.
 """
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser( description=DESCRIPTION, epilog=EPILOG )
-    parser.add_argument( 'exe_path', help='Full DOSBox path to the EXE (e.g. "C:\\\\WINDOWS\\\\PROGMAN.EXE")' )
+    parser.add_argument( 'module_path', help='Full DOSBox path to the EXE or DLL (e.g. "C:\\\\WINDOWS\\\\PROGMAN.EXE")' )
     parser.add_argument( 'mem_low', type=argparse.FileType( mode='rb' ), help='Memory dump of the low range: MEMDUMPBIN 0000 00000000 2000000' )
     parser.add_argument( 'mem_high', type=argparse.FileType( mode='rb' ), help='Memory dump of the high range: MEMDUMPBIN 0000 80000000 1000000' )
-    #parser.add_argument( '--offsets', help='Comma-seperated list of IDA offsets for each segment', required=False )
-    #parser.add_argument( '--in_file', type=argparse.FileType( mode='r' ), help='Input DOSBox LOGC data for processing', required=False )
-    #parser.add_argument( '--out_file', type=argparse.FileType( mode='w' ), help='Output file for Lighthouse in mod+off format', required=False )
+    parser.add_argument( '--out_file', type=argparse.FileType( mode='w' ), help='Output JSON file for segment information (default: stdout)', required=False )
     args = parser.parse_args()
 
-    exe_path = args.exe_path.upper().encode( 'cp1252' )+b'\x00'
+    module_path = args.module_path.upper()
+    module_path_raw = module_path.encode( 'cp1252' )+b'\x00'
     memory_map = {
         0x00000000: args.mem_low.read(),
         0x80000000: args.mem_high.read()
@@ -71,23 +72,30 @@ if __name__ == '__main__':
     # every Win16 app has one of these, which is very similar to the NE header
     # in the executable EXCEPT the segment table in this will tell us what
     # segments in the EXE are mapped to what LDT entry.
-    modtable_loc = find_module_table( memory_map, exe_path )
-    print( 'Module table for executable found at 0x{:08x}'.format( modtable_loc ) )
-
+    modtable_loc = find_module_table( memory_map, module_path_raw )
     modtable = get_module_table( memory_map, modtable_loc )
 
     #seg_map = {}
-    ida_list = []
     seg_list = []
     for i, s in enumerate( modtable.ne_header.segtable ):
-        ida_name = '{}seg{:02}'.format( 'c' if ldt_dir.seglist[s.selector >> 3].code_seg else 'd', i+1 )
-        seg_id = '{:04x}'.format( s.selector | 7 ).upper()
-        #seg_map[seg_id] = ida_name
-        seg_list.append( seg_id )
-        ida_list.append( ida_name )
-        print( '{} -> {:04x} -> {}'.format( i+1, s.selector | 7, ldt_dir.seglist[s.selector >> 3] ) )
+        ss = ldt_dir.seglist[s.selector >> 3]
+        seg_list.append( {
+            'index': i,
+            'dosbox_selector': '{:04x}'.format( s.selector | 7 ).upper(),
+            'ida_selector': '{}seg{:02}'.format( 'c' if ss.code_seg else 'd', i+1 ),
+            'base': '0x{:08x}'.format( ss.base ),
+            'limit': '0x{:05x}'.format( ss.limit ),
+            'is_code_segment': bool( ldt_dir.seglist[s.selector >> 3].code_seg ),
+            'is_present': bool( ldt_dir.seglist[s.selector >> 3].present ),
+        } )
+    result = {
+        'module': ntpath.split( module_path )[1],
+        'module_path': module_path,
+        'module_table_offset': '0x{:08x}'.format( modtable_loc ),
+        'segments': seg_list 
+    }
 
-    print( seg_list )
-    print( ida_list )
-
-    
+    if args.out_file:
+        json.dump( result, args.out_file, indent=4 )
+    else:
+        print( json.dumps( result, indent=4 ) )
